9fe6721c71bf6ec07beb63dcc21a9c18379d1dbd
TakehitoNishida <takehito.nishida@gmail.com>
2018-06-27 06:13:40 +0900

    update

--- example_databinding_main.py
+++ example_databinding_main.py
@@ -13,9 +13,9 @@ from example_databinding_viewmodel import Example_databinding_viewmodel
 class Example_databinding_main(Window):
 
     def __init__(self):
-        self.ui = wpf.LoadComponent(self, 'example_databinding_view.xaml')
         self.vm = Example_databinding_viewmodel()
-        self.ui.DataContext = self.vm
+        self.DataContext = self.vm
+        wpf.LoadComponent(self, 'example_databinding_view.xaml')
         print('Init window.')
 
 if __name__ == '__main__':
